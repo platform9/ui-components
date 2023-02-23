5a066bb75e52bd6ad54ecc14fecf10932a7a31b0
Kimberly Nguy <kimnguyy@gmail.com>
2023-02-23 15:12:42 -0800

    Remove theme selector. No use for it

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,37 @@
 import React from 'react'
-import makeStylesWithTheme from './theme-manager/makeStylesWithTheme'
-import Card from './components/card/Card'
-import Text from './components/Text'
-import Theme from './theme-manager/themes/model'
+import { Provider } from 'react-redux'
+import store from './store'
 
-const useStyles = makeStylesWithTheme((theme: Theme) => ({
-  testCard: {
-    backgroundColor: theme.palette.blue[300],
-  },
-  testText: {
-    color: theme.palette.red[300],
-  },
-}))
+// For testing purposes only
+// import Card from './components/card/Card'
+// import Text from './components/Text'
+// import makeStylesWithTheme from 'src/theme-manager/makeStylesWithTheme'
+// import Theme from 'src/theme-manager/themes/model'
 
-// FOR TESTING PURPOSES ONLY
-function App() {
-  const classes = useStyles()
+const App = () => {
+  // const classes = useStyles()
   return (
-    <>
-      <Card title="PF9 Card" className={classes.testCard} testId="test">
-        <div>
-          <Text variant="h2" className={classes.testText}>
+    <Provider store={store}>
+      {/* For testing only */}
+      {/* <div>
+        <Card title="PF9 Card" className={classes.card}>
+          <Text variant="h3" className={classes.text}>
             Test
           </Text>
-        </div>
-      </Card>
-    </>
+        </Card>
+      </div> */}
+    </Provider>
   )
 }
+
+// For testing only
+// const useStyles = makeStylesWithTheme((theme: Theme) => ({
+//   card: {
+//     backgroundColor: theme.palette.blue[300],
+//   },
+//   text: {
+//     color: theme.palette.red[300],
+//   },
+// }))
 
 export default App
