ee704b5549fce2dfee3e00bc78afc49cb13cc4a9
Kimberly Nguy <kimnguyy@gmail.com>
2023-06-23 15:39:59 -0700

    Support ExternalLink in NavItem

--- built/elements/sidebar/NavItem.d.ts
+++ built/elements/sidebar/NavItem.d.ts
@@ -11,5 +11,16 @@ interface Props extends IRouterLink {
         [key: string]: any;
     };
 }
+interface Props extends IRouterLink {
+    isActive?: boolean;
+    tooltip?: boolean;
+    open?: boolean;
+    compact?: boolean;
+    activeDisplayType?: 'background' | 'bar';
+    className?: string;
+    tooltipProps?: {
+        [key: string]: any;
+    };
+}
 export default function NavItem({ name, link, icon, className, open, isActive, compact, tooltip, tooltipProps, activeDisplayType, }: Props): JSX.Element;
 export {};
