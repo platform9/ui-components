7d144fac7ab588738f96e19b20cb0f19f851360f
Kimberly Nguy <kimnguyy@gmail.com>
2024-08-23 15:27:10 -0700

    Add dataTestId prop to Card component (#66)

--- built/elements/card/Card.d.ts
+++ built/elements/card/Card.d.ts
@@ -1,10 +1,11 @@
-import { ReactNode, PropsWithChildren } from 'react';
+import { PropsWithChildren, ReactNode } from 'react';
 export interface CardProps {
     title?: string | ReactNode;
     footer?: string | ReactNode;
     withCustomBody?: boolean;
     withCustomFooter?: boolean;
     className?: string;
+    dataTestId?: string;
 }
-declare function Card({ title, children, footer, withCustomBody, withCustomFooter, className, }: PropsWithChildren<CardProps>): JSX.Element;
+declare function Card({ title, children, footer, withCustomBody, withCustomFooter, className, dataTestId, }: PropsWithChildren<CardProps>): JSX.Element;
 export default Card;
