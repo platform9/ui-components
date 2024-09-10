827de46911fc0f9b2b9ee95d2b2f22238263aab1
Kimberly Nguy <kimnguyy@gmail.com>
2024-09-10 15:50:29 -0700

    Add dataTestId prop to Badge component (#68)
    * Add dataTestId prop to Badge component
    
    * fix typo

--- src/elements/badge/Badge.tsx
+++ src/elements/badge/Badge.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/styles'
-import Theme from '../../theme-manager/themes/model'
-import Tooltip, { TooltipProps } from '../../elements/tooltip/Tooltip'
+import React, { useState } from 'react'
 import { topMiddle } from '../../elements/menu/defaults'
+import Tooltip, { TooltipProps } from '../../elements/tooltip/Tooltip'
+import Theme from '../../theme-manager/themes/model'
 
+import clsx from 'clsx'
 import Text from '../../elements/Text'
 import { lightenDarkenColor, memoize } from '../../utils/misc'
 import generateTestId from '../../utils/test-helpers'
-import clsx from 'clsx'
 
 export type BadgeVariant =
   | 'default'
@@ -28,6 +28,7 @@ interface BadgeProps {
   tooltipBody?: React.ReactNode
   tooltipProps?: Partial<TooltipProps>
   className?: string
+  dataTestId?: string
 }
 const defaultTooltipProps = {
   ...topMiddle,
@@ -51,6 +52,7 @@ export default function Badge({
   tooltipBody = undefined,
   tooltipProps = defaultTooltipProps,
   className,
+  dataTestId = 'badge',
 }: BadgeProps) {
   const classes = useStyles({ variant })
   const [showAll, setShowAll] = useState(false)
@@ -58,7 +60,7 @@ export default function Badge({
   const textToShow = shouldTruncateText ? text.substring(0, ellipsisAt) : text
   return (
     <Tooltip {...tooltipProps} {...getTooltipProps(tooltipBody, text)}>
-      <div className={clsx(classes.badge, className)}>
+      <div className={clsx(classes.badge, className)} data-testid={dataTestId}>
         <Text
           data-testid={generateTestId(textToShow)}
           variant={bold ? 'caption1' : 'body2'}
