7417f3465252a900c6537251b8a4f345849f949b
pratik1499 <pratik.satpute@indexnine.com>
2025-11-21 12:11:56 +0530

    feat: enhance Input component with tooltip props
    - Added `tooltipProps` to the `InputProps` interface to allow customization of the Tooltip component.
    - Updated the Tooltip implementation in the Input component to spread `tooltipProps`, enabling more flexible tooltip configurations.

--- built/elements/input/Input.d.ts
+++ built/elements/input/Input.d.ts
@@ -1,4 +1,5 @@
 import React, { PropsWithChildren, ReactNode } from 'react';
+import { TooltipProps } from '../tooltip/Tooltip';
 export interface InputIconProps {
     onClick?: () => void;
     placement?: 'start' | 'end';
@@ -14,7 +15,8 @@ export interface InputProps extends React.DetailedHTMLProps<React.InputHTMLAttri
     info?: string | ReactNode;
     disabled?: boolean;
     error?: string;
+    tooltipProps?: TooltipProps;
     onChange?: (e: any) => void;
 }
-declare const Input: ({ className, compact, variant, mask, icon, iconProps, placeholder, label, info, children, value, onChange, disabled, error, ...rest }: PropsWithChildren<InputProps>) => JSX.Element;
+declare const Input: ({ className, compact, variant, mask, icon, iconProps, placeholder, label, info, children, value, onChange, disabled, error, tooltipProps, ...rest }: PropsWithChildren<InputProps>) => JSX.Element;
 export default Input;
